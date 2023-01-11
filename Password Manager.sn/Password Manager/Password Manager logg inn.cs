eecb57b1403fec8136c1cb6d4648f2e70063da84
AFiGhost <92977752+AFiGhost@users.noreply.github.com>
2023-01-11 11:10:44 +0100

    Create Password Manager logg inn.cs

---
+++ Password Manager.sn/Password Manager/Password Manager logg inn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Manager
+{
+    public partial class Logg_inn : Form
+    {
+        public Logg_inn()
+        {
+            InitializeComponent();
+        }
+
+        private void Logg_inn_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+
+
+        private void txtPassord_inn_logg_TextChanged(object sender, EventArgs e)
+        {
+            txtPassord_inn_logg.PasswordChar = (char)0x2022;
+        }
+
+        private void lblEnter_password_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
